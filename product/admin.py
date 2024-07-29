2a5de6f44317ac3e3035c81a1b0927435252413a
abhk343 <abhishekbaburaj.o3@gmail.com>
2024-07-29 23:00:29 +0530

    test

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Item)
+admin.site.register(Supplier)
+admin.site.register(Product)
+admin.site.register(Stock_in)
+
